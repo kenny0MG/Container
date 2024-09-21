be6a2bbb006d1da4be3ddab54b7544c1f8730cca
Kenny0MG <staspricepa144@gmail.com>
2024-09-21 19:23:40 +0300

    Проведен рефакторинг кода

--- AutoUchet/AutoUchet.cpp
+++ AutoUchet/AutoUchet.cpp
@@ -10,18 +10,22 @@ using namespace std;
 const int COUNT_MASSIVE = 6;
 
 
-
 int main() {
 
 	setlocale(LC_ALL, "Russian");
 
-	//first
+	//1
 	const vector<string> xs = { "hello", "appolon", "Ass", "computer", "aol"," "};
+
 	unsigned num_positive = count_if(xs.begin(), xs.end(), IsPositive);
+
 	cout << num_positive <<"\n";
 
 
-	//second
+
+
+
+	//2
 	vector<pair<unsigned, string>> person = { {12,"Ilya"s}, {23,"Dasha"s}, {56,"Nastya"s}, {12,"Anton"s} };
 
 	sort(person.begin(), person.end());
@@ -30,20 +34,29 @@ int main() {
 		cout << element.first << " " << element.second << endl;
 	}
 
+	
 
-	//third
+
+
+	//3
 	std::vector<double> responseTimes = { 2.5, 3.1, 4.0, 1.2, 3.5 };
 
 	double medianTime = calculateMedian(responseTimes);
+
 	std::cout << "Median response time: " << medianTime << std::endl;
   
 
 
-	//fourth
+
+
+
+	//4
 	vector<pair<int, string>> person_name;
 
 	int count;
+
 	cin >> count;
+
 	for (int i = 0; i < count; ++i) {
 		string name;
 		int age;
@@ -52,10 +65,6 @@ int main() {
 		cout << "age";
 		cin >> age;
 		person_name.push_back({ age,name });
-
-
-
-		
 	}
 	sort(person_name.begin(), person_name.end());
 
@@ -65,8 +74,11 @@ int main() {
 
 
 
-	//fifth
+
+
+	//5
 	int massive[COUNT_MASSIVE];
+
 	cout << "Введите " << COUNT_MASSIVE << " элементов массива:\n";
 
 	for (int i = 0; i < COUNT_MASSIVE; ++i) {
@@ -78,38 +90,42 @@ int main() {
 		cout << massive[i] << "\t";
 	}
 
-	//sixsth
+
+
+
+
+
+	//6
 	vector<int> numbers;
 	int count_number, number;
 
 	cout << "Введите колличество элементов массива:\n";
-
-
 	cin >> count;
 	cout << "Элементы массива:\n";
-
 
 	for (int i = 0; i != count; ++i) {
 		cin >> number;
 		numbers.push_back(number);
 	}
-
 	double sr = arifm(numbers);
 
 	cout<<sr;
 
 
-	//seventh
+
+
+	//7
 	set<string> stop_words = { "of", "in"};
 	string query = "Hi my name is Stas";
 	string add = accumulate(stop_words.begin(), stop_words.end(), query, AddStopWords);
-
 
 	cout << add;
 
 
 
-	//eight
+
+
+	//8
 	set<string> set_words = { "Stas","Leon","Victor" };
 	vector<string> vector_words = { "Stas","j","Leon","Iliya", };
 
